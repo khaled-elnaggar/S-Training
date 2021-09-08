87a29dc2e0a6185acf1fbe95c482471d6f2e0fe6
Khalid <dev_khalid@hotmail.com>
2021-09-08 22:04:43 +0200

    Add basic functionality to hide and show login msg

---
+++ login.js
@@ -0,0 +1,7 @@
+var successMessage = document.getElementById('success-message');
+
+const logUserIn = () => {
+  console.log('hiiiiiii');
+  console.log(successMessage);
+  successMessage.style.display = "block";
+}
