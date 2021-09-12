079a19299681e99b63e7da0e8ef4b6f14af8d81b
Khalid <dev_khalid@hotmail.com>
2021-09-12 14:10:18 +0200

    Implement a score system and modify level gen

--- js/index.js
+++ js/index.js
@@ -1,38 +1,57 @@
 const LEFT_CARD = document.getElementById('left-card');
 const RIGHT_CARD = document.getElementById('right-card');
-const EMOJIS = ["😂", "🙃", "😇", "🤪", "😜", "🥺"]
+const EMOJIS = ["😇", "🙃", "🤪", "😂", "😜", "🥺"]
 
 const LEVEL = {
-  _currLevel: 3,
+  _currLevel: 9,
   incLevel: () => this._currLevel++,
-  getImageCount() { return this._currLevel * 6 },
+  getImageCount() { return this._currLevel * 8 },
   getRandomCount() { return 1 },
-  getImageSize() { return 100 - (10 * this._currLevel) }
+  getImageSize() { return Math.max(40, 100 - (10 * this._currLevel)) },
+  getEmoji() { return (this._currLevel < 5) ? EMOJIS[this._currLevel - 1] : EMOJIS[parseInt(EMOJIS.length * Math.random())] },
+  resetLevel() { this._currLevel = 1 }
 }
 
+const SCORE = {
+  _score: 0,
+  incScore() { this._score += 10 },
+  decScore() { this._score -= 5 },
+  resetScore() { this._score = 0 }
+}
 
 const imagesFactory = {
-  getAnImage(emoji) {
+  _genAnImage() {
+    const offset = LEVEL.getImageSize() / 5;
 
-    const left = Math.random() * (100 - LEVEL.getImageSize() / 5) + "%";
-    const top = Math.random() * (100 - LEVEL.getImageSize() / 5) + "%";
+    const left = Math.random() * (100 - offset) + "%";
+    const top = Math.random() * (100 - offset) + "%";
 
     const image = document.createElement('span');
     image.style.left = left;
     image.style.top = top;
     image.style.fontSize = LEVEL.getImageSize() + "px";
     image.style.position = "absolute";
-    image.innerHTML = emoji;
+    image.style.cursor = "crosshair"
+    image.innerHTML = LEVEL.getEmoji();
     return image;
   },
 
   genLeftAndRight() {
-    const imageLeft = this.getAnImage(EMOJIS[parseInt(EMOJIS.length * Math.random())]);
+    const imageLeft = this._genAnImage();
+    imageLeft.addEventListener('click', () => { SCORE.decScore() });
 
     LEFT_CARD.appendChild(imageLeft);
+
+
     let imageRight = imageLeft.cloneNode();
     imageRight.innerHTML = imageLeft.innerHTML;
     RIGHT_CARD.appendChild(imageRight);
+  },
+
+  genRandomLeft() {
+    const randomImage = this._genAnImage();
+    randomImage.addEventListener('click', () => { SCORE.incScore() });
+    LEFT_CARD.appendChild(randomImage);
   }
 }
 
@@ -40,4 +59,8 @@ const imagesFactory = {
 
 for (var i = 0; i < LEVEL.getImageCount(); i++) {
   imagesFactory.genLeftAndRight()
+}
+
+for (var i = 0; i < LEVEL.getRandomCount(); i++) {
+  imagesFactory.genRandomLeft();
 }
