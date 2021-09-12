b3dfc8c1f5249ac8acf142c881aa800ef57d42b5
Khalid <dev_khalid@hotmail.com>
2021-09-12 14:15:25 +0200

    Fix style

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const RIGHT_CARD = document.getElementById('right-card');
 const EMOJIS = ["😇", "🙃", "🤪", "😂", "😜", "🥺"]
 
 const LEVEL = {
-  _currLevel: 9,
+  _currLevel: 1,
   incLevel: () => this._currLevel++,
   getImageCount() { return this._currLevel * 8 },
   getRandomCount() { return 1 },
@@ -14,6 +14,7 @@ const LEVEL = {
 
 const SCORE = {
   _score: 0,
+  getScore() { return this._score },
   incScore() { this._score += 10 },
   decScore() { this._score -= 5 },
   resetScore() { this._score = 0 }
@@ -39,18 +40,16 @@ const imagesFactory = {
   genLeftAndRight() {
     const imageLeft = this._genAnImage();
     imageLeft.addEventListener('click', () => { SCORE.decScore() });
-
     LEFT_CARD.appendChild(imageLeft);
-
-
     let imageRight = imageLeft.cloneNode();
     imageRight.innerHTML = imageLeft.innerHTML;
+    imageRight.addEventListener('click', () => { SCORE.decScore() });
     RIGHT_CARD.appendChild(imageRight);
   },
 
   genRandomLeft() {
     const randomImage = this._genAnImage();
-    randomImage.addEventListener('click', () => { SCORE.incScore() });
+    randomImage.addEventListener('click', (e) => { SCORE.incScore(); LEFT_CARD.removeChild(e.target) });
     LEFT_CARD.appendChild(randomImage);
   }
 }
